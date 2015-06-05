0c0280173452eb9e0f282ac2900dee49b8849af1
Latyshev Grigory <grigorii.latyshev@cern.ch>
2015-06-05 17:13:39 +0200

    fix

--- index.php
+++ index.php
@@ -5,13 +5,13 @@
 <center>
 <h1>FE STATUS BITS</h1>
 <a href="downloads">ROOT files</a><br>
-<a href="Plots">PLots</a><br>
+<a href="Plots">Plots</a><br>
 
 
 <?php
 
 foreach (scandir("plots") as $s){
- if ($s == "." or $s == ".." or $s != ".htaccess"){
+ if ($s == "." or $s == ".." or $s == ".htaccess"){
   continue;
  }
  echo "<img src='plots/$s'><br>";
